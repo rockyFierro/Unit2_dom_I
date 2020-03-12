da4ea21ba078040541fd42e554929ee2eaa0bdbc
Rocky Fierro <39335873+rockyFierro@users.noreply.github.com>
2020-03-12 02:50:01 -0700

    commenting finished , 1 minor bug.

--- js/index.js
+++ js/index.js
@@ -37,27 +37,54 @@ const siteContent = {
   },
 };
 
+
+
+
+
+//////////////////////////////////////////////////////////////
+////****************    LET'S CODE!!     ***************//////
+//////////////////////////////////////////////////////////////
+
+const selector = arg => document.querySelector(arg);
+const selectorAll = arg => document.querySelectorAll(arg);
+
 // Example: Update the img src for the logo
 //edited for continuity and length
-let logo = document.getElementById("logo-img").src = siteContent.nav["img-src"];
+
+let logo = selector("#logo-img").src = siteContent.nav["img-src"];
 
 //navigation bar variables
-const navSource = Object.values(siteContent.nav);
+// first the source
+// second the location the data will be sent
+
+const navSource = Object.values(siteContent.nav); 
 const navbar = document.querySelectorAll("a");
 
+
 //call to action variables and assignment
+//declare the htag, where it is and where its data is
+//declare the button, where it is and where its data is
+//declare the image, where it is and where its data is
+
 const hTag = document.querySelector('h1').textContent = siteContent.cta.h1;
 const button = document.querySelector('button').innerHTML = siteContent.cta.button;
 const ctaImage = document.getElementById('cta-img').src = siteContent.cta["img-src"];
 
 //main content top content
-const selector = arg => document.querySelector(arg);
-const selectorAll = arg => document.querySelectorAll(arg);
+//declare an array to hold all the headings
+//declare an array to hold all the articles
 
 const articleHeading = selectorAll('.text-content h4');
 const articleContent = selectorAll('.text-content p');
 
+//shorten out work a little with an alias
+
 const mainSource = siteContent["main-content"];
+
+//TODO:
+//Find the iteration that will shuffle the source and the reference
+//together to shorten this dictionary... 
+//function returning reference did not work.
 
 articleHeading[0].textContent = mainSource["features-h4"];
 articleContent[0].textContent = mainSource["features-content"];
@@ -71,19 +98,45 @@ articleHeading[4].textContent = mainSource["vision-h4"];
 articleContent[4].textContent = mainSource["vision-content"];
 const codeImg = selector("#middle-img").src = mainSource["middle-img-src"];
 
+
+//declare heading and info for contact area
+
 const contactInfoHeading = selector(".contact h4");
 const contactInfo = selectorAll(".contact p");
 
+//set info for contact heading
+
 contactInfoHeading.textContent = siteContent.contact["contact-h4"];
+
+//TODO: 
+//Use previous solution to article problem here
 
 contactInfo[0].textContent = siteContent.contact.address;
 contactInfo[1].textContent = siteContent.contact.phone;
 contactInfo[2].textContent = siteContent.contact.email;
 
+//footer - a pleasure to work with you; declare and set
+
 const footerCR = selector("footer p").textContent = siteContent.footer.copyright;
-//get that navbar up
+
+
+//cursed loop
+//if this is moved to its original spot
+//on line 62 everything breaks.
+
 navSource.forEach((link, index) => {
   navbar[index].textContent = link;
   navbar[index].setAttribute("src", link);
   navbar[index].style.color = "green";
 });
+
+//cursed loop is preventing newcomers
+//lets make these new links fast
+
+const newlink1 = document.createElement("a"),
+      newlink2 = document.createElement("a");
+newlink1.innerText = "HELLO";
+newlink2.innerText = "WORLD";
+const parentEl = selector("nav");
+parentEl.appendChild(newlink2);
+parentEl.prepend(newlink1);
